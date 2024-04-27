70f06a57ae35f359d14984f171fd688e6032fea9
dpapadat94 <dennis.papadatos94@gmail.com>
2024-04-27 17:12:59 -0400

    final

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+// TODO: import router from users.route
+import userRouter from "./users.route";
+
+const router = express.Router();
+
+router.get("/test", (req, res) => {
+  res.send("working");
+});
+
+// TODO: use the imported router to handle all routes matching "/users"
+router.use("/users", userRouter);
+
+export default router;
